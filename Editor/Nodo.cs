8d192a8da3f62726564ccebf01b6f44050b6cf72
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2022-11-14 23:15:13 -0300

    Agregando la estructura para poder hacer puntos adaptables

---
+++ Editor/Nodo.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+namespace ItIsNotOnlyMe.AnimacionesAdaptables
+{
+    public abstract class Nodo<TTipo> : INodo<TTipo>
+    {
+        protected List<IInfluencia<TTipo>> _influencias;
+
+        public Nodo()
+        {
+            _influencias = new List<IInfluencia<TTipo>>();
+        }
+
+        public void AgregarInfluencia(IInfluencia<TTipo> influencia)
+        {
+            _influencias.Add(influencia);
+        }
+
+        public abstract TTipo EstadoActual();
+    }
+}
